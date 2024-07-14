a17a56915c4eb53ad5282be27a466cf868f502b2
m790101 <m790101@hotmail.com>
2024-07-14 19:16:23 +0800

    add logo and path healthz

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ func main() {
 	port := "8080"
 	mux := http.NewServeMux()
 
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
