75c05792056b20f6061287e991d0817e91f18b4f
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2024-08-07 11:15:32 +0800

    add sorting option to url feature

--- handleGetChirps.go
+++ handleGetChirps.go
@@ -4,18 +4,53 @@ import (
 	"log"
 	"net/http"
 	"server/internal/database"
+	"sort"
 	"strconv"
 )
 
 func (cfg *apiConfig) handleGetChirps(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	db, _ := database.NewDB("./database.json")
-	chirps, err := db.GetChirps()
+	// db, _ := database.NewDB("./database.json")
+	chirps, err := cfg.Db.GetChirps()
 	if err != nil {
 		responseWithError(w, http.StatusInternalServerError, "Error getting chirps")
 		return
 	}
+
+	sortingOption := r.URL.Query().Get("sort")
+
+	// default sorting
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].ID < chirps[j].ID
+	})
+
+	if sortingOption == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].ID > chirps[j].ID
+		})
+	}
+
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err := strconv.Atoi(authorIDString)
+		if err != nil {
+			respondWithJSON(w, http.StatusBadRequest, "Invalid author ID")
+			return
+		}
+		filteredChirps := []database.Chirp{}
+		for index, chirp := range chirps {
+			if chirp.AuthorId == authorID {
+				filteredChirps = append(filteredChirps, chirps[index])
+			}
+		}
+		// sort.Slice(filteredChirps, func(i, j int) bool {
+		// 	return chirps[i].ID < chirps[j].ID
+		// })
+		respondWithJSON(w, http.StatusOK, filteredChirps)
+		return
+	}
+
 	respondWithJSON(w, http.StatusOK, chirps)
 
 }
